7f6fb7949cf25dae13b7a7745b34864463b2c596
Fabiano Brito <fabianopbrito@gmail.com>
2016-04-11 14:18:52 +0000

    Sets the CRUD actions for user's bank account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,10 +1,46 @@
 class AccountsController < ApplicationController
   
   before_action :confirm_logged_in
+  before_action :find_user
   
-  def show
+  def new
+    @account = Account.new({:user_id => @user.id})
+  end
+  
+  def create
+    @account = Account.new(account_params)
+    if @account.save
+      flash[:notice] = "Account successfuly created"
+      redirect_to :controller => 'users', :action => 'show', :id => @user.id
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @account = Account.find(params[:id])
   end
+  
+  def update
+    @account = Account.find(params[:id])
+    if @account.update_attributes(account_params)
+      flash[:notice] = "Account was successfully updated"
+      redirect_to :controller => 'users', :action => 'show', :id => @account.user_id
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  
+  def account_params
+    params.require(:account).permit(:user_id, :social_security, :phone_number, :iban, :swift, :address, :postal_code, :city, :project)
+  end
+  
+  def find_user
+    if params[:user_id]
+      @user = User.find(params[:user_id])
+    end
+  end
+  
 end
