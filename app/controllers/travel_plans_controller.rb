27ac08b6ef708d752961b3ac6b2d195844d04568
Fabiano Brito <fabianopbrito@gmail.com>
2016-04-29 09:17:38 +0000

    Refactors travel_plans controller to be more DRY friendly.

--- app/controllers/travel_plans_controller.rb
+++ app/controllers/travel_plans_controller.rb
@@ -1,15 +1,11 @@
 class TravelPlansController < ApplicationController
-  # before_action :set_travel_plan, only: [:show, :edit, :update, :destroy]
   
   before_action :confirm_logged_in
   before_action :find_user
+  before_action :set_travel_plan, only: [:show, :edit, :update, :delete]
 
   def index
     @travel_plans = @user.travel_plans.newest_first
-  end
-
-  def show
-    @travel_plan = TravelPlan.find(params[:id])
   end
 
   def new
@@ -24,31 +20,21 @@ class TravelPlansController < ApplicationController
       render 'new'
     end
   end
-  
-  def edit
-    @travel_plan = TravelPlan.find(params[:id])
-  end
-
+ 
   def update
-    @travel_plan = TravelPlan.find(params[:id])
     if @travel_plan.update_attributes(travel_plan_params)
       redirect_to :action => 'show', :user_id => @user.id, :id => @travel_plan.id
     else
       render 'edit'
     end
   end
-  
-  def delete
-    @travel_plan = TravelPlan.find(params[:id])
-  end
-
+ 
   def destroy
     TravelPlan.find(params[:id]).destroy
     redirect_to :action => 'index', :user_id => @user.id
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_travel_plan
       @travel_plan = TravelPlan.find(params[:id])
     end
