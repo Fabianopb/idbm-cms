cb9e4664f6724702427718e3bf22cdcb96e673a7
Fabiano Brito <fabianopbrito@gmail.com>
2016-05-02 11:12:08 +0000

    Adds persistence token to users table.

---
+++ db/migrate/20160502111001_adds_persistence_token_to_users.rb
@@ -0,0 +1,5 @@
+class AddsPersistenceTokenToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :persistence_token, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160502075814) do
+ActiveRecord::Schema.define(version: 20160502111001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -146,8 +146,9 @@ ActiveRecord::Schema.define(version: 20160502075814) do
     t.string   "e_mail"
     t.string   "password_digest"
     t.string   "role"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.string   "persistence_token"
   end
 
 end
