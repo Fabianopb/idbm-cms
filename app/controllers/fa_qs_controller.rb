b1cfbb0fd91dae1e68fe7c978bcb5c58377cfb89
Fabiano Brito <fabianopbrito@gmail.com>
2016-05-04 14:24:53 +0000

    Sets up FAQs controller and model.

--- app/controllers/fa_qs_controller.rb
+++ app/controllers/fa_qs_controller.rb
@@ -1,60 +1,46 @@
 class FaQsController < ApplicationController
-  before_action :set_faq, only: [:show, :edit, :update, :destroy]
   
   before_action :confirm_logged_in
-
-  # GET /faqs
+  before_action :set_faq, only: [:edit, :update, :delete]
+  
   def index
-    @faqs = Faq.all
+    @faqs = Faq.sorted
   end
-
-  # GET /faqs/1
-  def show
-  end
-
-  # GET /faqs/new
+  
   def new
     @faq = Faq.new
   end
-
-  # GET /faqs/1/edit
-  def edit
-  end
-
-  # POST /faqs
+  
   def create
     @faq = Faq.new(faq_params)
-
     if @faq.save
-      redirect_to @faq, notice: 'Faq was successfully created.'
+      redirect_to :action => 'index'
     else
-      render :new
+      render 'new'
     end
   end
-
-  # PATCH/PUT /faqs/1
+  
   def update
-    if @faq.update(faq_params)
-      redirect_to @faq, notice: 'Faq was successfully updated.'
+    if @faq.update_attributes(faq_params)
+      redirect_to :action => 'index'
     else
-      render :edit
+      render 'edit'
     end
   end
 
-  # DELETE /faqs/1
   def destroy
-    @faq.destroy
-    redirect_to faqs_url, notice: 'Faq was successfully destroyed.'
+    Faq.find(params[:id]).destroy
+    redirect_to :action => 'index'
   end
-
+  
   private
-    # Use callbacks to share common setup or constraints between actions.
+  
     def set_faq
       @faq = Faq.find(params[:id])
     end
-
-    # Only allow a trusted parameter "white list" through.
+    
     def faq_params
-      params[:faq]
+      params.require(:faq).permit(:question, :answer, :position)
     end
+    
 end
--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,2 +1,7 @@
 class Faq < ActiveRecord::Base
+    
+    acts_as_list
+    
+    scope :sorted, lambda { order("faqs.position ASC") }
+    
 end
