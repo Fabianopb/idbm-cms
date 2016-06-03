e1d8749d80c04e2d6b6ce34c5c5838e19c1db2fa
Fabiano Brito <fabianopbrito@gmail.com>
2016-06-03 14:27:29 +0000

    Materializes receipts inside refund claims.

--- app/models/daily_allowance.rb
+++ app/models/daily_allowance.rb
@@ -2,5 +2,5 @@ class DailyAllowance < ActiveRecord::Base
     belongs_to :refund_claim
     belongs_to :user
     
-    scope :sort_by_arrival, lambda { order("daily_allowances.arrival DESC") }
+    scope :sort_by_arrival, lambda { order("daily_allowances.arrival ASC") }
 end
