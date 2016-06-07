b8a86c867fc3811cac7f19d2aadfe651f1db7471
Fabiano Brito <fabianopbrito@gmail.com>
2016-06-07 14:14:47 +0000

    Implementing bootstrap forms for travel plans.

--- app/assets/javascripts/effects.js
+++ app/assets/javascripts/effects.js
@@ -1,6 +1,16 @@
 $(function(){
     $(document).ready(function() {
         
+        $('.datetimepicker').datetimepicker();
+        
+        $('#datepicker1').datetimepicker( {
+            format: 'L'
+        });
+        
+        $('#datepicker2').datetimepicker( {
+            format: 'L'
+        });
+        
         $('#trip-info-form').hide();
         
         $('#trip-info-switch').on('change', function() {
