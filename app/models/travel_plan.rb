7a36546bcfb5af539258b218e8cd86de796da9d1
Fabiano Brito <fabianopbrito@gmail.com>
2016-07-05 16:44:12 +0000

    Working on travel plans views before moving to local.

--- app/models/travel_plan.rb
+++ app/models/travel_plan.rb
@@ -8,4 +8,12 @@ class TravelPlan < ActiveRecord::Base
     def travellers
         self.users.pluck(:first_name).join('; ')
     end
+    
+    def total_allowance
+        paid_days * daily_allowance
+    end
+    
+    def total_costs
+        tickets_cost + total_allowance + accommodation_costs + events_costs + other_costs
+    end
 end
