57245db4e7ecd52f0cfb883e44649c645091e877
fabiano <fabianopbrito@gmail.com>
2016-07-19 22:26:32 +0300

    Styles refund claims views - not claim elements.

--- app/assets/javascripts/effects.js
+++ app/assets/javascripts/effects.js
@@ -1,20 +1,23 @@
 $(function(){
     $(document).ready(function() {
+
+        // Initialize Bootstrap's tooltip
+        $('[data-toggle="tooltip"]').tooltip();
         
-        $('.datetimepicker').datetimepicker();
+        // $('.datetimepicker').datetimepicker();
         
-        $('#datepicker1').datetimepicker( {
-            format: 'L'
-        });
+        // $('#datepicker1').datetimepicker( {
+        //     format: 'L'
+        // });
         
-        $('#datepicker2').datetimepicker( {
-            format: 'L'
-        });
+        // $('#datepicker2').datetimepicker( {
+        //     format: 'L'
+        // });
         
-        $('#trip-info-form').hide();
+        // $('#trip-info-form').hide();
         
-        $('#trip-info-switch').on('change', function() {
-            $('#trip-info-form').toggle($('#trip-info-switch').prop('checked'));
-        });
+        // $('#trip-info-switch').on('change', function() {
+        //     $('#trip-info-form').toggle($('#trip-info-switch').prop('checked'));
+        // });
   });
 });
