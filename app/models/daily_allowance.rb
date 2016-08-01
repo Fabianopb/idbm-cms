4463c6df1b0060f54faa41475575351162b4e988
fabiano <fabianopbrito@gmail.com>
2016-08-01 21:55:06 +0300

    Form validations for the Daily Allowance model.

--- app/models/daily_allowance.rb
+++ app/models/daily_allowance.rb
@@ -1,6 +1,18 @@
 class DailyAllowance < ActiveRecord::Base
-    belongs_to :refund_claim
-    belongs_to :user
-    
-    scope :sort_by_arrival, lambda { order("daily_allowances.arrival ASC") }
+  
+  belongs_to :refund_claim
+  belongs_to :user
+  
+  scope :sort_by_arrival, lambda { order("daily_allowances.arrival ASC") }
+
+  validates :destination, 
+  					:arrival,
+  					presence: true
+
+  validate :arrival_after_departure
+
+  def arrival_after_departure
+  	errors.add(:arrival, "must be after #{self.refund_claim.departure} and before #{self.refund_claim.return_date}") if arrival < self.refund_claim.departure || arrival > self.refund_claim.return_date
+  end
+
 end
