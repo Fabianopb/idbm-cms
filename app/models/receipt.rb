c8de3f4cf06690af6472b8257249711e373c4947
fabiano <fabianopbrito@gmail.com>
2016-08-01 22:07:56 +0300

    Form validations for the Receipt model.

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,15 +1,14 @@
 class Receipt < ActiveRecord::Base
     
-    belongs_to :refund_claim
-    belongs_to :user
-    
-    scope :newest_first, lambda { order("receipts.created_at DESC") }
+  belongs_to :refund_claim
+  belongs_to :user
+  
+  scope :newest_first, lambda { order("receipts.created_at DESC") }
 
-    # validates :description, presence: true
-    # validate :trip_dates
-
-    # def trip_dates
-    # 	errors.add
-    # end
+  validates :expense_date,
+  					:description,
+  					:value,
+  					:currency,
+  					presence: true 			
 
 end
