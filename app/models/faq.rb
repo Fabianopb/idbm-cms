9d3332a40ef7cc55556ec12c2c530c8c42154871
fabiano <fabianopbrito@gmail.com>
2016-08-02 21:18:00 +0300

    Form validations for the Faq model.

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,7 +1,11 @@
 class Faq < ActiveRecord::Base
     
-    acts_as_list
-    
-    scope :sorted, lambda { order("faqs.position ASC") }
+  acts_as_list
+  
+  scope :sorted, lambda { order("faqs.position ASC") }
+
+  validates :question,
+  					:answer,
+  					presence: true
     
 end
