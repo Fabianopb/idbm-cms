ae896da08be945a789f686c9ba380bb60b8d2bce
fabiano <fabianopbrito@gmail.com>
2016-08-07 20:08:38 +0300

    By user creation password and username are generated automatically.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,20 @@
 module ApplicationHelper
+	
 	def default_statuses
 		['not submitted', 'sent for approval', 'requires revision', 'approved']
 	end
+
+	def status_color(status)
+		case status
+		when default_statuses[0]
+		  'current-status-red'
+		when default_statuses[1]
+		  'current-status-yellow'
+		when default_statuses[2]
+		  'current-status-red'
+		when default_statuses[3]
+		  'current-status-green'
+		end
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,31 +14,46 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :projects
 
 
+  before_validation :set_username, on: :create
+  before_validation :set_password, on: :create
+
   validates :first_name,
             :last_name,
+            :e_mail,
+            :role,
             presence: true
   
   validates :username, presence: true, uniqueness: true
 
-  validates :e_mail,
-            :birth_date,
-            :role,
-            presence: true
+  validates :birth_date, presence: true, :on => :update
 
   attr_accessor :enable_strict_validation
   validate :presence_of_passwords, :if => :enable_strict_validation
+
+  def set_username
+    self.username = "#{self.e_mail}"
+  end
+
+  def set_password
+    o = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map { |i| i.to_a }.flatten
+    self.password = (0...8).map { o[rand(o.length)] }.join
+    self.password_confirmation = self.password
+  end
 
   def presence_of_passwords
     errors.add(:password, "can't be blank") if password.nil?
   end
 
+
   acts_as_authentic do |c|
     c.crypto_provider = Authlogic::CryptoProviders::BCrypt
   end
   
+
   scope :sorted, -> { order("users.role ASC", "users.first_name ASC") }
   scope :all_except, ->(user) { where(["id != ?", user.id]) }
   
+
   def name
     "#{first_name} #{last_name}"
   end
