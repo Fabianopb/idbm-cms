b25842c5850fe5b645fd6e66efb892ce071db230
Fabiano Brito <fabianopbrito@gmail.com>
2016-08-31 22:08:18 +0300

    Moves ActionMailer configuration to their respective environment config files.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,33 +1,15 @@
 class ApplicationMailer < ActionMailer::Base
-	default from: 'idbm321@gmail.com'
+	default from: 'IDBM Programme'
 
   def welcome_email(user)
     @user = user
     @url  = 'https://safe-earth-52111.herokuapp.com/'
-    delivery_options = {
-		  :address              => "smtp.sendgrid.net",
-		  :port                 => '587',
-		  :authentication       => :plain,
-		  :user_name            => ENV['SENDGRID_USERNAME'],
-		  :password             => ENV['SENDGRID_PASSWORD'],
-		  :domain               => 'heroku.com',
-		  :enable_starttls_auto => true  
-		}
-    mail(to: @user.email, subject: 'Welcome to IDBM Industry Project!', delivery_method_options: delivery_options)
+    mail(to: @user.email, subject: 'Welcome to IDBM Industry Project!')
   end
 
   def reset_email(user)
   	@user = user
-  	delivery_options = {
-		  :address              => "smtp.sendgrid.net",
-		  :port                 => '587',
-		  :authentication       => :plain,
-		  :user_name            => ENV['SENDGRID_USERNAME'],
-		  :password             => ENV['SENDGRID_PASSWORD'],
-		  :domain               => 'heroku.com',
-		  :enable_starttls_auto => true  
-		}
-    mail(to: @user.email, subject: 'IDBM Password reset instructions', delivery_method_options: delivery_options)
+    mail(to: @user.email, subject: 'IDBM Password reset instructions')
   end
 
 end
